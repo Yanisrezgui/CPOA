a8af4d8a08a35424420088d517feebfcca9c1e57
Newfel19 <newfellevrel01@gmail.com>
2021-10-07 20:59:19 +0200

    commit main

--- src/td1/Main.java
+++ src/td1/Main.java
@@ -1,5 +1,15 @@
 package td1;
 
-public class Main {
+import dao.mysql.MySQLClientDAO;
+import modele.Client;
 
+public class Main {
+	
+	Connexion connexion = new Connexion();
+	
+	MySQLClientDAO MC = new MySQLClientDAO();
+	
+	Client c1 = new Client(1, "Rezgui", "Yanis", 3, "rue du chaudron", "52422", "Paris", "France" );
+	Client c2 = new Client(2, "Batman", "Louis", 7, "rue de la loire", "36232", "Metz", "France" );
+	
 }
