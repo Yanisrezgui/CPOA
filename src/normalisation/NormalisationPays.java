bff917e4fdcebb69c37eb436044d0c496f12d24f
Yanisrezgui <chayano57@gmail.com>
2021-10-08 09:30:27 +0200

    methode normalisation pays

--- src/normalisation/NormalisationPays.java
+++ src/normalisation/NormalisationPays.java
@@ -1,5 +1,28 @@
 package normalisation;
 
 public class NormalisationPays {
+	
+	public static String normPays(String pays) {
+		
+		String s1="belgique";
+		String s2="luxembourg";
+		String s3="Suisse";
+		
+		pays=pays.trim();
+		
+		if(pays.equalsIgnoreCase("letzebuerg"))
+		{
+			return s2;
+		}
+		else if(pays.equalsIgnoreCase("belgium"))
+		{
+			return s1;
+		}
+		else if(pays.equalsIgnoreCase("Switzerland")||(pays.equalsIgnoreCase("Schweiz")))
+		{
+			return s3;
+		}
+		else return pays;
+	}
 
 }
