34e472e40700eca2dd299f98236bbd39571b402c
Yanisrezgui <chayano57@gmail.com>
2021-10-15 08:06:22 +0200

    il me demande de le faire mais j'ai rien changer

--- src/normalisation/NormalisationVille.java
+++ src/normalisation/NormalisationVille.java
@@ -15,22 +15,22 @@ public class NormalisationVille {
 		// replaceall() remplace lès- en -lès-
 		
 		ville=ville.replaceAll("-lès ","-lès-");		ville=ville.replaceAll(" le ","-le-");
-		ville=ville.replaceAll(" lès-", "-lès-");	ville=ville.replaceAll("-le ", "-le-");
-		ville=ville.replaceAll(" lès ","-lès-");	ville=ville.replaceAll(" le-", "le");
+		ville=ville.replaceAll(" lès-", "-lès-");		ville=ville.replaceAll("-le ", "-le-");
+		ville=ville.replaceAll(" lès ","-lès-");		ville=ville.replaceAll(" le-", "le");
 		
 		ville=ville.replaceAll("-les ","-lès-");		ville=ville.replaceAll(" à ","-à-");	
-		ville=ville.replaceAll(" les-", "-lès-");	ville=ville.replaceAll("-à ","-à-");	
-		ville=ville.replaceAll(" les ","-lès-");	ville=ville.replaceAll(" à-","-à-");	
+		ville=ville.replaceAll(" les-", "-lès-");		ville=ville.replaceAll("-à ","-à-");	
+		ville=ville.replaceAll(" les ","-lès-");		ville=ville.replaceAll(" à-","-à-");	
 		
 		
-		ville=ville.replaceAll(" a ","-à-");		ville=ville.replaceAll("-aux ", "-aux-");	
+		ville=ville.replaceAll(" a ","-à-");			ville=ville.replaceAll("-aux ", "-aux-");	
 		ville=ville.replaceAll(" a-","-à-");			ville=ville.replaceAll(" aux ", "-aux-");
 		ville=ville.replaceAll("-a ","-à-");			ville=ville.replaceAll(" aux-", "-aux-");
 		
 		
-		ville=ville.replaceAll("-sous ","-sous-");	ville=ville.replaceAll("-sur","-sur-");
-		ville=ville.replaceAll(" sous ", "-sous-");	ville=ville.replaceAll(" sur ", "-sur-");
-		ville=ville.replaceAll(" sous-", "-sous-");	ville=ville.replaceAll("sur-", "-sur-");
+		ville=ville.replaceAll("-sous ","-sous-");		ville=ville.replaceAll("-sur","-sur-");
+		ville=ville.replaceAll(" sous ", "-sous-");		ville=ville.replaceAll(" sur ", "-sur-");
+		ville=ville.replaceAll(" sous-", "-sous-");		ville=ville.replaceAll("sur-", "-sur-");
 		
 		
 		return ville;
