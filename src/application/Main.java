73b1915b67111b1d1d2bee7d95962577fe45e46a
Yanisrezgui <chayano57@gmail.com>
2021-10-15 11:34:21 +0200

    oui

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,37 @@
 package application;
 
+import java.lang.Exception;
+import java.net.URL;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.fxml.FXMLLoader;
+
 public class Main extends Application{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+	try {
+	URL fxmlURL=getClass().getResource("/fenetre.fxml");
+	FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL);
+	Node root = fxmlLoader.load();
+	Scene scene = new Scene((VBox) root, 600, 400);
+	scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+	primaryStage.setScene(scene);
+	primaryStage.setTitle("Ma première fenêtre JavaFX");
+	primaryStage.show();
+	} catch (Exception e) {
+	e.printStackTrace();
+	}
 	}
 
 }
